2b55ee40de30709f25205b51bab7fb29ff161259
Duncan Mac-Vicar P <dmacvicar@suse.de>
2012-06-11 15:56:57 +0200

    move the test init to a setup method

--- test/test_widget.rb
+++ test/test_widget.rb
@@ -3,28 +3,26 @@ require 'ui'
 
 class WidgetTest < Test::Unit::TestCase
 
-  def setup    
+  def setup
+    @dialog = UI::Builder.create_main_dialog
+    @widget = UI::Builder.create_push_button(@dialog, "Ok")
   end
 
   # Test ruby values as object ids
   def test_id
-    dialog = UI::Builder.create_main_dialog
-    widget = UI::Builder.create_push_button(dialog, "Ok")
+    
+    assert !@widget.has_id?
+    assert_equal nil, @widget.id
 
-    assert !widget.has_id?
+    @widget.id = :foo
 
-    assert_equal nil, widget.id
-
-    widget.id = :foo
-
-    assert widget.has_id?
-
-    assert_equal :foo, widget.id
+    assert @widget.has_id?
+    assert_equal :foo, @widget.id
 
     obj = Object.new
-    widget.id = obj
-    assert_equal obj, widget.id
-    assert_not_equal Object.new, widget.id
+    @widget.id = obj
+    assert_equal obj, @widget.id
+    assert_not_equal Object.new, @widget.id
   end
 
 
