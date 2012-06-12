5703d583385ec92771b640ed632b5105454dabaf
Duncan Mac-Vicar P <dmacvicar@suse.de>
2012-06-12 11:21:16 +0200

    * Add object tracking * Make sure Dialog throws if Dialog#destroy! is called, instead of   segfault (YDialog::destroy() deletes the ptr)

--- ext/ui/widget.cc
+++ ext/ui/widget.cc
@@ -1,12 +1,13 @@
 #include <yui/YWidgetID.h>
 
 #include "widget.h"
+#include "widget_object_map.h"
 #include "ruby_value_widget_id.h"
 
 static void
 dealloc(YWidget *wg)
 {
-
+  std::cout << "destroy widget" << std::endl;
 }
 
 VALUE
@@ -15,18 +16,36 @@ ui_wrap_widget(YWidget *dlg)
   return Data_Wrap_Struct(cUIWidget, NULL, dealloc, dlg);
 }
 
+static YWidget *
+ui_unwrap_widget(VALUE wdg)
+{
+  YWidget *ptr = 0L;
+  Data_Get_Struct (wdg, YWidget, ptr);
+  if (!ptr) 
+    rb_raise(rb_eRuntimeError, "Widget was already destroyed!");
+  return ptr;
+}
+
 VALUE
 each_child(VALUE self)
 {
-    YWidget *ptr = 0L;
-    Data_Get_Struct(self, YWidget, ptr);
+    YWidget *ptr = ui_unwrap_widget(self);
     
     for (YWidgetListConstIterator it = ptr->childrenBegin();
          it != ptr->childrenEnd();
          ++it) {
-      std::cout << *it << std::endl;
+      YWidget *ptr = *it;
+      rb_yield(widget_object_map_for(ptr));
+      //std::cout << *it << std::endl;
     }
     return Qnil;
+}
+
+VALUE
+children_count(VALUE self)
+{
+    YWidget *ptr = ui_unwrap_widget(self);
+    return INT2NUM(ptr->childrenCount());
 }
 
 /** 
@@ -35,8 +54,7 @@ each_child(VALUE self)
 VALUE
 id(VALUE self)
 {
-    YWidget *ptr = 0L;
-    Data_Get_Struct(self, YWidget, ptr);
+    YWidget *ptr = ui_unwrap_widget(self);
     RubyValueWidgetID *id = dynamic_cast<RubyValueWidgetID *>(ptr->id());
     return id ? id->rubyValue() : Qnil;
 }
@@ -44,8 +62,7 @@ id(VALUE self)
 VALUE
 set_id(VALUE self, VALUE id)
 {
-    YWidget *ptr = 0L;
-    Data_Get_Struct(self, YWidget, ptr);
+    YWidget *ptr = ui_unwrap_widget(self);
     ptr->setId(new RubyValueWidgetID(id));
     return id;
 }
@@ -53,16 +70,14 @@ set_id(VALUE self, VALUE id)
 VALUE
 has_id(VALUE self)
 {
-    YWidget *ptr = 0L;
-    Data_Get_Struct(self, YWidget, ptr);
+    YWidget *ptr = ui_unwrap_widget(self);
     return ptr->hasId() ? Qtrue : Qfalse;
 }
 
 VALUE
 is_valid(VALUE self)
 {
-    YWidget *ptr = 0L;
-    Data_Get_Struct(self, YWidget, ptr);
+    YWidget *ptr = ui_unwrap_widget(self);
     return ptr->isValid() ? Qtrue : Qfalse;
 }
 
@@ -79,6 +94,7 @@ void init_ui_widget()
   rb_define_method(klass, "id=", C_FUNC(set_id), 1);
   rb_define_method(klass, "has_id?", C_FUNC(has_id), 0);
   rb_define_method(klass, "each_child", C_FUNC(each_child), 0);
+  rb_define_method(klass, "children_count", C_FUNC(children_count), 0);
   rb_define_method(klass, "valid?", C_FUNC(is_valid), 0);
 }
 
