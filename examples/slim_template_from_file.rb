25a90a1ca39749ceb2bcce63cda1fd253bb98221
Josef Reidinger <jreidinger@suse.cz>
2012-06-12 09:03:40 +0200

    destroy first dialog after event

--- examples/slim_template_from_file.rb
+++ examples/slim_template_from_file.rb
@@ -8,6 +8,7 @@ TEMPLATE_FILE2 = File.expand_path("../wizard2.uit",__FILE__)
 dialog = UI.slim(File.read(TEMPLATE_FILE))
 dialog.wait_for_event
 puts dialog.id
+dialog.destroy!
 dialog2 = UI.slim(File.read(TEMPLATE_FILE2))
 dialog2.wait_for_event
 puts dialog2.id
