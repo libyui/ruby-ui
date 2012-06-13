8cbc1894163f19353e91c1d52930cbc0e5a5b975
Duncan Mac-Vicar P <dmacvicar@suse.de>
2012-06-13 09:57:37 +0200

    formatting

--- ext/ui/event.cc
+++ ext/ui/event.cc
@@ -5,19 +5,21 @@
 VALUE convert_event(const YEvent * event)
 {
   const YDialog * dialog = event->dialog();
+
   VALUE rb_dialog = Qnil;
-  if (dialog)
-  {
+
+  if (dialog) {
     rb_dialog = widget_object_map_for(dialog);
   }
-  VALUE ui = rb_define_module("UI");
+
+    VALUE ui = rb_define_module("UI");
   VALUE result = Qnil;
+
   switch (event->eventType())
   {
   case YEvent::WidgetEvent:
-  {
     const YWidgetEvent* we = static_cast<const YWidgetEvent*>(event);
-    VALUE cls = rb_const_get(ui,rb_intern("WidgetEvent"));
+    VALUE cls = rb_const_get(ui, rb_intern("WidgetEvent"));
     VALUE reason = Qnil;
     switch (we->reason())
     {
@@ -39,34 +41,27 @@ VALUE convert_event(const YEvent * event)
       rb_raise(rb_eNotImpError, "Unknown reason in event");
     }
     VALUE widget = widget_object_map_for(we->widget());
-    result = rb_funcall(cls,rb_intern("new"),3,widget,reason,rb_dialog);
+    result = rb_funcall(cls, rb_intern("new"), 3, widget, reason, rb_dialog);
     break;
-  }
   case YEvent::KeyEvent:
-  {
     const YKeyEvent* we = static_cast<const YKeyEvent*>(event);
-    VALUE cls = rb_const_get(ui,rb_intern("KeyEvent"));
+    VALUE cls = rb_const_get(ui, rb_intern("KeyEvent"));
     VALUE widget = widget_object_map_for(we->focusWidget());
     VALUE key = ID2SYM(rb_intern(we->keySymbol().c_str()));
-    result = rb_funcall(cls,rb_intern("new"),3,key,widget,rb_dialog);
+    result = rb_funcall(cls, rb_intern("new"), 3, key, widget, rb_dialog);
     break;
-  }
   case YEvent::CancelEvent:
-  {
     const YCancelEvent* we = static_cast<const YCancelEvent*>(event);
-    VALUE cls = rb_const_get(ui,rb_intern("CancelEvent"));
-    result = rb_funcall(cls,rb_intern("new"),1,rb_dialog);
+    VALUE cls = rb_const_get(ui, rb_intern("CancelEvent"));
+    result = rb_funcall(cls, rb_intern("new"), 1, rb_dialog);
     break;
-  }
   case YEvent::TimeoutEvent:
-  {
     const YTimeoutEvent* we = static_cast<const YTimeoutEvent*>(event);
     VALUE cls = rb_const_get(ui,rb_intern("TimeoutEvent"));
-    result = rb_funcall(cls,rb_intern("new"),1,rb_dialog);
+    result = rb_funcall(cls, rb_intern("new"), 1, rb_dialog);
     break;
-  }
   default: //TODO
-      rb_raise(rb_eNotImpError, (std::string("Unknown event type ")+YEvent::toString(event->eventType())).c_str());
+      rb_raise(rb_eNotImpError, (std::string("Unknown event type ") + YEvent::toString(event->eventType())).c_str());
   }
   return result;
 }
