bedbda7f548a64a608edfc0a5bc31721096263a9
Duncan Mac-Vicar P <dmacvicar@suse.de>
2012-08-06 11:33:12 +0200

    widget gallery example, based on libyui's one

---
+++ examples/widget_gallery.rb
@@ -0,0 +1,56 @@
+$: << File.expand_path(File.join(File.dirname(__FILE__), '../lib'))
+require 'ui'
+require 'ui/builder/slim'
+require 'pp'
+#require 'debugger'
+
+dialog = UI.main_dialog {
+  vbox {
+    # row 1
+    hbox(:id => :hbox1) {
+      frame("PushButton", :Weight => 1) {
+        hvcenter {
+          vbox {
+            push_button "Enabled"
+            push_button "Disabled", :Enabled => false
+          }
+        }
+      }
+      frame("Label", :Weight => 1) {
+        hvcenter {
+          vbox {
+            label "Headline"
+            label "Label"
+          }
+        }
+      }
+      frame("CheckBox", :Weight => 1) {
+        hvcenter {
+          vbox {
+            check_box "Check0"
+            check_box "Check1"
+            check_box "Disabled", :Enabled => false
+          }
+        }
+      }
+      frame("RadioButton", :Weight => 1) {
+        hvcenter {
+          radio_button_group {
+            vbox {
+              radio_button "Radio0"
+              radio_button "Radio1"
+              radio_button "Disabled", :Enabled => false
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+count = 0
+dialog.wait_for_event do |ev|
+  break if ev.cancel? || count > 10
+  count += 1
+  true
+end
